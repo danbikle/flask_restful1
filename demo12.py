94ad83e53c1bf2e7777c305d0d9254215f478164
Dan Bikle <bikle@bikle.com>
2017-01-24 11:25:16 -0800

    hello

--- demo12.py
+++ demo12.py
@@ -18,7 +18,7 @@ import flask_restful as fr
 application = flask.Flask(__name__)
 api         = fr.Api(application)
 
-class Demo12(fr.Resource):
+class Demo12a(fr.Resource):
   # I should tell get() about URL-path parameters:
   def get(self, tkr='AAPL', yr2predict=2016, yrs2train=25):
     k1_s = 'You want to predict'
@@ -26,9 +26,9 @@ class Demo12(fr.Resource):
     k3_s = 'By learning from this many years'
     return {k1_s:tkr, k2_s:yr2predict, k3_s:yrs2train}
 # I should declare URL-path parameters:
-api.add_resource(Demo12, '/demo12/<tkr>/<yr2predict>/<yrs2train>')
+api.add_resource(Demo12a, '/demo12a/<tkr>/<yr2predict>/<yrs2train>')
 
-class Demo13(fr.Resource):
+class Demo12b(fr.Resource):
   # I should tell get() about URL-path parameters:
   def get(self, tkr='AAPL', yr2predict=2016, yrs2train=25):
     k1_s = '1. You want to predict'
@@ -36,7 +36,7 @@ class Demo13(fr.Resource):
     k3_s = '3. By learning from this many years'
     return {k1_s:tkr, k2_s:yr2predict, k3_s:yrs2train}
 # I should declare URL-path parameters, and I should constrain them:
-api.add_resource(Demo13, '/demo13/<tkr>/<int:yr2predict>/<int:yrs2train>')
+api.add_resource(Demo12b, '/demo12b/<tkr>/<int:yr2predict>/<int:yrs2train>')
 
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))
