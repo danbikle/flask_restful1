b42aae4b14c446fecb905e19fea74df3698987c0
Dan Bikle <bikle@bikle.com>
2017-01-25 12:29:43 -0800

    more

--- demo15.py
+++ demo15.py
@@ -10,7 +10,7 @@
 # Demo:
 # export FLASK_DEBUG=1
 # ~/anaconda3/bin/python demo15.py
-# curl localhost:5000/demo15/IBM/9/2017-12-31
+# curl localhost:5000/demo15/IBM/2016/9/
 import pdb
 import os
 import flask         as fl
@@ -25,18 +25,16 @@ api         = fr.Api(application)
 
 class Demo15(fr.Resource):
   # I should tell get() about URL-path-tokens:
-  def get(self, tkr='AAPL', yrs2train=8, date2predict='2018-01-01'):
+  def get(self, tkr='AAPL', yr2predict='2017', yrs2train=8):
     k1_s   = '1. You want to predict'
-    k2_s   = '2. For this date'
+    k2_s   = '2. For this year'
     k3_s   = '3. By learning from this many years'
     k4_s   = '4. With '
-    algo_s = 'Logistic Regression'
-    k5_s   = '5. And the prediction is '
-    k6_s   = '6. Most recent price is '
+    algo_s = 'Linear Regression'
     return {'under': 'construction'}
 # I should declare URL-path-tokens, and I should constrain them:
-api.add_resource(Demo15, '/demo15/<tkr>/<int:yrs2train>')
-# curl localhost:5000/demo15/IBM/9
+api.add_resource(Demo15, '/demo15/<tkr>/<yr2predict>/<int:yrs2train>')
+# curl localhost:5000/demo15/SPY/2017/1/
 
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))
